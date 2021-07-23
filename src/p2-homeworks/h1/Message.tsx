314ccdc1b50a7cc2a6cb5e28a377ffa86e2718bc
Ivan Vorobev <superhd@icloud.com>
2021-07-23 04:41:14 +0300

    task done

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import m from "./Message.module.css"
+
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={m.wrap}>
+            <div className={m.avatarPositioning}>
+                <img src={props.avatar} alt="" className={m.pic}/>
+                <div className={m.bubble}>
+                    <div className={m.bubbleContent}>
+                        <div className={m.name}>{props.name}</div>
+                        <div className={m.message}>{props.message}</div>
+                    </div>
+                    <div className={m.time}>{props.time}</div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
